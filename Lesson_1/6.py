ed6aa545eefb7a23533d88ac5197b37fd82f79f3
Armine <aldalinde@gmail.com>
2019-01-12 19:58:39 +0300

    Task 6 Lesson 1

--- Lesson_1/6.py
+++ Lesson_1/6.py
@@ -1,1 +1,5 @@
 # 6.	Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+number_letter = int(input("Choose a letter and enter its number: "))
+
+print(string.ascii_lowercase[number_letter -1])
