3c1bd07b40742a18b6218f6fe066a8a4119bc5ae
Armine <aldalinde@gmail.com>
2019-01-12 19:58:26 +0300

    Task 5 Lesson 1

--- Lesson_1/5.py
+++ Lesson_1/5.py
@@ -1,1 +1,9 @@
 #5.	Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят, и сколько между ними находится букв.
+import string
+
+letter1 = input("Enter the first letter ")
+letter2 = input("Enter the second letter ")
+ind_1 = string.ascii_lowercase.index(letter1)
+ind_2 = string.ascii_lowercase.index(letter2)
+print("Number of letters between given: ")
+print(string.ascii_lowercase.index(letter2) - string.ascii_lowercase.index(letter1) - 1)
