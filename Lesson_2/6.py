33e072fb75b2adaacdf6958901ac29976bb457ac
Armine <aldalinde@gmail.com>
2019-01-17 17:05:45 +0300

    task 6 lesson 2 done

--- Lesson_2/6.py
+++ Lesson_2/6.py
@@ -5,9 +5,38 @@
 число, чем то, что загадано. Если за 10 попыток число не отгадано,
 то вывести загаданное число.
 """
-from ramdom import randint
-
-number = randint(0,100)
+from random import randint
 
 print("Угадайте число от 0 до 100 за 10 попыток")
+
+def guess(number, i):
+    tries = 11 - i
+    print(f"Попыток осталось {tries}")
+    n = int(input(f"Введите число "))
+    print(number)
+    if n == number:
+        print("Вы угадали!")
+        return number
+    elif i == 10:
+        print("Вы проиграли")
+        return number
+    else:
+        i += 1
+        print("Не угадали")
+
+        while True:
+            if n < number:
+                print(f"Число {n} меньше загаданного")
+                a = guess(number, i)
+            elif n > number:
+                print(f"Число {n} больше загаданного")
+                a = guess(number, i)
+            print()
+            return a
+
+
+number = randint(0, 100)
+result = guess(randint(0, 100), 1)
+
+print(f"Загаданное число было {result}")
 
