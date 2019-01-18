3eae6fc3f105f3b84ce990c1f2d922d142bca9bc
Armine <aldalinde@gmail.com>
2019-01-18 18:50:21 +0300

    задание 5 урок 3

--- Lesson_3/5.py
+++ Lesson_3/5.py
@@ -1,1 +1,14 @@
 #5.	В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию (индекс) в массиве.
+
+from random import randint
+
+
+# making a list of 10 random integers in range - 10 : 10
+rand_num_list = []
+while len(rand_num_list) < 10:
+    rand_num_list.append(randint(-10 , 10))
+print(rand_num_list)
+
+negatives = [n for n in rand_num_list if n < 0]
+
+print(f"Максимальное отрицательное число {max(negatives)} имеет в ряду индекс {rand_num_list.index(max(negatives))}")
