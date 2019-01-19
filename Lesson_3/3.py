908b1aec4901966d3260a2de025cd1bce937d281
Armine <aldalinde@gmail.com>
2019-01-20 00:22:50 +0300

    Revert "Merge commit '1b2ab795fde9bff4f22ee28e5d5d736695198e5f'"
    This reverts commit 9bbd3b36f8c960edca366a0e94bacb64a66e71fa, reversing
    changes made to ff735ae56d485a3beb033347d5fa37ced6ae1889.

--- Lesson_3/3.py
+++ Lesson_3/3.py
@@ -1,12 +1,1 @@
 #3.	В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
-import random
-
-numbers = [j for j in range(2, 100)]
-random.shuffle(numbers)
-list1 = numbers[:9]
-print(list1)
-a = list1.index(max(list1))
-b = list1.index(min(list1))
-list1[a], list1[b] = list1[b], list1[a]
-
-print(list1)
