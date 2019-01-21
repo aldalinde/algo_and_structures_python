fe82955e5fe7261c5564d7970da76d10332a848d
Armine <aldalinde@gmail.com>
2019-01-21 13:07:44 +0300

    задание 1 урок 2. проверены алгоритмы из задачи 7 урока 2

--- Lesson_4/1-2.py
+++ Lesson_4/1-2.py
@@ -4,79 +4,46 @@
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 
 Урок 2:
-4.	Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
-Количество элементов (n) вводится с клавиатуры.
+
+7.	Напишите программу, доказывающую или проверяющую, что для множества
+натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
+ где n - любое натуральное число.
+
 """
 
-
+from random import randint
 import timeit
 
 
-n = int(input("Давайте найдем сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ... Выберите, чему равно n "))
-i = 1
-number = 1
-# вычисляем, какое число будет n-ным в последовательности 1, -0.5 , 0,25 ....
+#Способ №1 рекурсия
 
-#Способ №1 через цикл
-def loop(n, number, i):
-    list_num = [1]
-    while i < n:
-        number = number / -2
-        list_num.append(number)
-        i += 1
-    return sum(list_num)
+def eq_check():
 
-print(loop(n, number, i))
+    def sum_up(n, last):
+        if n == last:
+            return n
+        else:
+            return n + sum_up(n + 1, last)
 
-print(timeit.timeit(("loop(n, number, i)"), setup="from __main__ import loop, n, number, i"))
-# при  n = 6 время выполнения пишет 5.8 секунд
+    last = randint(1, 100)
+    n = 1
+    return sum_up(n, last) == last*(last+1)/2
 
 
-#Способ №2 алгоритм как он был написан ко второму уроку: цикл + рекурсия, но время только рекурсии (( -
-while i < n:
-    number = number / -2
-    i += 1
-last_number = number
-print(last_number)
+print(eq_check())
+print(timeit.timeit(("eq_check()"), setup="from __main__ import eq_check"))
+# время выполнения пишет больше 58 секунд, хотя результат на print(eq_check()) выдает очень быстро. Почему?
 
 
-# выход из алгоритма на условии else,
-# когда первый аргумент становится равен n-ному числу последовательности
-def num_sum(number, last_number):
-    if number != last_number:
-        return number + num_sum(number/ -2, last_number)
-    else:
-        return number
-# вызываем функцию от 1 и уже определенного нами ранее n-нного числа последовательности
-#print(num_sum(1, last_number))
-print(timeit.timeit(("num_sum(1, last_number)"), setup="from __main__ import num_sum, last_number"))
-# при  n = 6 время выполнения пишет 4 секунды. Но при этом первый цикл while как я понимаю, в это время не входит.
+#Способ №2 
 
 
-#Способ №3 Учитываем и цикл по нахождению n-ного числа в ряду, и рекурсию
+def check():
+    num = randint(1, 100)
+    num_list = [n for n in range(1, num+1)]
+    return sum(num_list) == int(num*(num+1)/2)
 
+print(check())
 
-def loop_2(n, number, i):
-    while i < n:
-        number = number / -2
-        i += 1
-    return number
-
-def num_sum_2(number, last_number):
-    if number != last_number:
-        return number + num_sum(number/ -2, last_number)
-    else:
-        return number
-
-
-n = int(input("Давайте найдем сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ... Выберите, чему равно n "))
-i = 1
-number = 1
-
-# вызываем функцию от 1 и уже определенного нами ранее n-нного числа последовательности (результат функции-цикла)
-print(num_sum_2(1, loop_2(n, number, i)))
-
-
-
-print(timeit.timeit(("num_sum(1, loop_2(n, number, i))"), setup="from __main__ import num_sum, loop_2, n, number, i"))
-# при  n = 6 время выполнения пишет 8 секунд.
+print(timeit.timeit(("check()"), setup="from __main__ import check"))
+# время выполнения пишет больше 22 секунд, хотя результат на print(check()) выдает тоже очень быстро. Почему?
