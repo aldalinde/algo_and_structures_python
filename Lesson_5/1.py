c1f0003bcb460169cad86c78147794aa04b6592c
Armine <aldalinde@gmail.com>
2019-01-26 04:08:08 +0300

    Task 1 Lesson 5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,30 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+fin_report = {}
+comp_number = int(input("Общее число предприятий: "))
+
+for n in range(comp_number):
+    comp_name = input(f"Наименование {n + 1}-го предприятия: ")
+    profit_1_quater = float(input("Прибыль за 1-й квартал: "))
+    profit_2_quater = float(input("Прибыль за 2-й квартал: "))
+    profit_3_quater = float(input("Прибыль за 3-й квартал: "))
+    profit_4_quater = float(input("Прибыль за 4-й квартал: "))
+    fin_report[comp_name] = profit_1_quater + profit_2_quater + profit_3_quater + profit_4_quater
+
+average_profit = sum(fin_report.values()) / comp_number
+
+print("Средняя годовая прибыль предприятий %s составляет %.2f" % (list(fin_report.keys()), average_profit))
+
+better_doing = []
+worse_doing = []
+
+for company in fin_report:
+    if fin_report[company] > average_profit:
+        better_doing.append(company)
+    elif fin_report[company] < average_profit:
+        worse_doing.append(company)
+
+print(f"Компании с прибылью выше средней: {better_doing}")
+print(f"Компании с прибылью ниже средней: {worse_doing}")
